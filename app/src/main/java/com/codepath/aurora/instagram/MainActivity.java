3bc0f841dff8f28d547a4de235059ded2c5c6433
Aurora Pérez <35460259+AuroraPrz18@users.noreply.github.com>
2021-07-07 15:55:21 -0500

    3. User can log out

--- app/src/main/java/com/codepath/aurora/instagram/MainActivity.java
+++ app/src/main/java/com/codepath/aurora/instagram/MainActivity.java
@@ -1,8 +1,13 @@
 package com.codepath.aurora.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu resource
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    /***
+     * Method called when the LogOut item inside de menu is clicked. It signed back out the user.
+     * @param item
+     */
+    public void onClickLogOut(MenuItem item) {
+        ParseUser.logOut(); // Log out the currently logged in user session
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
